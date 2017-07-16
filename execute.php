d94718ec7fcfb248f8fe59281018e2326cc73bee
fdilillo <f.dilillo@esconsulting.it>
2017-07-16 12:02:53 +0200

    fix camel case and add a reply for /start command

--- execute.php
+++ execute.php
@@ -10,14 +10,19 @@ if(!$update)
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$firstName = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastName = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
 
 $text = trim($text);
 $text = strtolower($text);
+
+switch ($text)
+{
+    case '/start' : $text = 'hello ' . $firstName;
+}
 
 header("Content-Type: application/json");
 $parameters = array('chat_id' => $chatId, "text" => $text);
