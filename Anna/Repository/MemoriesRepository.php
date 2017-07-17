d2a3c9b1f8fdcaa788c831451aea919d603b58f8
fdilillo <f.dilillo@esconsulting.it>
2017-07-17 12:22:08 +0200

    test

--- Anna/Repository/MemoriesRepository.php
+++ Anna/Repository/MemoriesRepository.php
@@ -30,6 +30,9 @@ class MemoriesRepository extends PostgreSQLConnector
             'chat_id' => $data['chat_id']
         ]);
 
+        error_log( $memoryResult );
+        echo $memoryResult;
+        die;
         $crateString = "insert into user_memory (user_id, memory_id) values (" .
             array_shift($userResult)['id'] . ", " . array_shift($memoryResult)['id'] . ");";
 
